c9b94a2d33a6c83e54f35a111e199fe0fe68244e
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-07 12:00:49 +0700

    hash password in model of mongodb

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); // Erase if already required
-
+const bcrypt = require('bcrypt');
 // Declare the Schema of the Mongo model
 var userSchema = new mongoose.Schema({
     firstName: {
@@ -64,5 +64,19 @@ var userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+//trong model cuar mongodb thi khong the dung arrow function 
+// vi this khong the su dung trong arrow function
+//nhung api nao khong phai update ma luu lai gia tri nhu create, save 
+// thi se chay vao trong doan code nay
+userSchema.pre('save', async function (next) {
+    //neu password da hash roi thi khong hash no nua
+    //neu password khong thay doi thi se goi ham next() =>dong vai tro nhu return
+    //no se chay ham tiep theo ma khong thuc hien doan code ben duoi
+    if (!this.isModified('password')) {
+        next();
+    }
+    const salt = bcrypt.genSaltSync(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 //Export the model
 module.exports = mongoose.model('User', userSchema);
