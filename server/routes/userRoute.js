31c0711ee511180ca8a8aa7808d54c57506a8b32
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-16 22:02:00 +0700

    #4 update api

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,15 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
+const { verifyAccessToken } = require('../middlewares/verifyToken');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.login);
+router.post('/refreshtoken', userController.refreshAccessToken)
+router.get('/logout', userController.logout)
+router.get('/current', verifyAccessToken, userController.getCurrentUser)
+router.get('/forgotpassword', userController.forgotPassword)
+router.put('/resetpassword', userController.resetPassword)
+
+
 
 module.exports = router;
