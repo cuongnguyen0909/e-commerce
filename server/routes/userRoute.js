070cedb65a4f9d9a4373ee4fa97433d8d9906ca8
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-18 17:00:07 +0700

    add middleware isAdmin for Admin role

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,14 +1,20 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 const { verifyAccessToken } = require('../middlewares/verifyToken');
+const isAdmin = require('../middlewares/isAdmin');
 
 router.post('/register', userController.registerUser);
 router.post('/login', userController.login);
 router.post('/refreshtoken', userController.refreshAccessToken)
 router.get('/logout', userController.logout)
-router.get('/current', verifyAccessToken, userController.getCurrentUser)
+router.get('/current', [verifyAccessToken], userController.getCurrentUser)
+router.get('/', [verifyAccessToken, isAdmin], userController.getAllUser)
 router.get('/forgotpassword', userController.forgotPassword)
 router.put('/resetpassword', userController.resetPassword)
+router.delete('/', [verifyAccessToken, isAdmin], userController.deleteUser)
+router.put('/current', verifyAccessToken, userController.updateUser)
+router.put('/:uid', [verifyAccessToken, isAdmin], userController.updateUserByAdmin)
+
 
 
 
