8bb13480b203805a2e328d6f555ea8891ef87099
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-11-30 22:20:25 +0700

    add coupon feature (controller, model, route )

---
+++ server/routes/couponRoute.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const couponController = require('../controllers/couponController');
+const { verifyAccessToken } = require('../middlewares/verifyToken');
+const isAdmin = require('../middlewares/isAdmin');
+
+router.post('/', [verifyAccessToken, isAdmin], couponController.createCoupon)
+router.get('/', couponController.getCoupons)
+router.put('/:cid', [verifyAccessToken, isAdmin], couponController.updateCoupon)
+router.delete('/:cid', [verifyAccessToken, isAdmin], couponController.deleteCoupon)
+
+module.exports = router;
