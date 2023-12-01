6047e51dc7f9f1fcd25763fb140c3ed46c6a1781
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-01 22:18:07 +0700

    client use data from store redux toolkit

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import { createSlug } from '../ultils/helpers';
 
 const SideBar = () => {
+    const { categories } = useSelector(state => state.app);
+    // console.log(categories);
+    // const { isLoading } = useSelector(state => state.app);
+    // console.log(isLoading);
     return (
-        <div>SideBar</div>
+        <div className='flex flex-col border'>
+            {categories?.map(item => (
+                <NavLink
+                    key={createSlug(item.title)}
+                    to={createSlug(item.title)}
+                    className={({ isActive }) => isActive ? 'bg-main text-white px-5 pt-[15px] pb-[14px] text- hover:text-main'
+                        : 'px-5 pt-[15px] pb-[14px] text-sm hover:text-main'}
+                >
+                    {item?.title}
+                </NavLink>
+            ))}
+        </div>
     )
-}
+};
 
-export default SideBar
+export default SideBar;
