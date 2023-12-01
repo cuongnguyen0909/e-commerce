c8376665d7a3d5d4d4c34d36b2ba20dead1c63a2
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-01 22:17:04 +0700

    client banner component

--- client/src/components/Banner.jsx
+++ client/src/components/Banner.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
 
 const Banner = () => {
     return (
-        <div>Banner</div>
-    )
-}
+        <div className="w-full ">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvrvYxh3xBUf_vLjBHJXwulE9s6GxL1kviFA&usqp=CAU"
+                alt="Banner"
+                className="h-[400px] w-full object-cover"
+            />
+        </div>
+    );
+};
 
-export default Banner
+export default Banner;
