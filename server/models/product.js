9184d4c3ffd47f870598bc00701b64455f251bd5
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-01 22:12:52 +0700

    add thumb product in model

--- server/models/product.js
+++ server/models/product.js
@@ -1,61 +1,70 @@
 const mongoose = require('mongoose'); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var productSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        trim: true, //bo dau cach 2 dau
+var productSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true, //bo dau cach 2 dau
+        },
+        slug: {
+            //may tinh apple -> may-tinh-apple
+            type: String,
+            required: true,
+            lowercase: true,
+        },
+        description: {
+            type: Array,
+            required: true,
+        },
+        brand: {
+            type: String,
+            required: true,
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        category: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+            default: 0,
+        },
+        sold: {
+            // so luong hang da ban
+            type: Number,
+            default: 0,
+        },
+        thumb: {
+            type: String,
+            required: true
+        },
+        images: {
+            type: Array,
+        },
+        color: {
+            type: String,
+        },
+        ratings: [
+            {
+                star: { type: Number },
+                postedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
+                comment: { type: String },
+            },
+        ],
+        totalRatings: {
+            type: Number,
+            default: 0,
+        },
     },
-    slug: {//may tinh apple -> may-tinh-apple
-        type: String,
-        required: true,
-        lowercase: true
+    {
+        timestamps: true,
     },
-    description: {
-        type: Array,
-        required: true,
-    },
-    brand: {
-        type: String,
-        required: true,
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    category: {
-        type: String,
-        required: true,
-    },
-    quantity: {
-        type: Number,
-        default: 0,
-    },
-    sold: { // so luong hang da ban
-        type: Number,
-        default: 0,
-    },
-    images: {
-        type: Array,
-    },
-    color: {
-        type: String,
-    },
-    ratings: [
-        {
-            star: { type: Number },
-            postedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
-            comment: { type: String }
-        }
-    ],
-    totalRatings: {
-        type: Number,
-        default: 0
-    }
-}, {
-    timestamps: true
-});
+);
 
 //Export the model
 module.exports = mongoose.model('Product', productSchema);
