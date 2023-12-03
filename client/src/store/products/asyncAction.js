27f9dc944026b9c0a6377d074be48a24a2fae2da
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-03 23:15:01 +0700

    action of product and categories

---
+++ client/src/store/products/asyncAction.js
@@ -0,0 +1,13 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import * as apis from '../../apis';
+
+
+export const getNewProduct = createAsyncThunk('product/newProducts', async (data, { rejectWithValue }) => {
+    const response = await apis.apiGetProducts({ sort: '-createdAt', 'totalRatings[gt]': 3 });
+    console.log(response);
+    if (!response.success) {
+        return rejectWithValue(response);
+    }
+    return response.products;
+})
+
