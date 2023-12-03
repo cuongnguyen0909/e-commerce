625c5a35601a2b24405e1cee8942a6a275846324
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-03 23:17:01 +0700

    Define a few more paths

--- client/src/ultils/path.js
+++ client/src/ultils/path.js
@@ -3,10 +3,13 @@ const path = {
     HOME: '',
     ALL: '*',
     LOGIN: 'login',
-    PRODUCTS: 'products',
-    BLOGS: 'blogs',
-    OUR_SERVICES: 'services',
+    PRODUCT: 'product',
+    BLOG: 'blog',
+    SERVICE: 'service',
     FAQS: 'faqs',
+    DETAIL_PRODUCT__PID__TITLE: 'product/:pid/:title',
+    DETAIL_PRODUCT: 'product'
+
 }
 
 export default path;
