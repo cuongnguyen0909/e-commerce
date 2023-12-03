8b9cdafbefea381d2d564e063d44c58314e9becd
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-03 23:14:37 +0700

    product slice (redux toolkit)

---
+++ client/src/store/products/productSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from '@reduxjs/toolkit';
+import * as actions from '../products/asyncAction';
+
+export const productSlice = createSlice({
+    name: 'product',
+    initialState: {
+        newProduct: null,
+        errorMesaage: ''
+    },
+    reducers: {
+        // logout: (state) => {
+        //     state.isLoading = false;
+        // }
+    },
+    extraReducers: (builder) => {
+        // start action(promise pending)
+        builder.addCase(actions.getNewProduct.pending, (state) => {
+            state.isLoading = true;
+        });
+
+        builder.addCase(actions.getNewProduct.fulfilled, (state, action) => {
+            // console.log(action);
+            state.isLoading = false;
+            state.newProduct = action.payload;
+        })
+
+        builder.addCase(actions.getNewProduct.rejected, (state, action) => {
+            state.isLoading = false;
+            state.errorMesaage = action.payload.message;
+        })
+    }
+})
+// export const { } = appSlice.actions; /// nam trong reducers
+export default productSlice.reducer;
