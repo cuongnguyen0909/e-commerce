4f1b831921487f4d1835c4d1105cea80717f117e
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-03 23:14:18 +0700

    format money, render star from number, exchange seconds to hours minute ...

--- client/src/ultils/helpers.js
+++ client/src/ultils/helpers.js
@@ -1,3 +1,35 @@
+import icons from './icons';
+const { FaRegStar, FaStar } = icons;
+
 export const createSlug = (str) => {
     return str.toString().toLowerCase().normalize('NFD').replace(/[\u0300-\u036f]/g, '').split(' ').join('-');
+}
+
+export const formatMoney = (number) => {
+    return (+number).toLocaleString('vi-VN');
+}
+
+export const renderStarFromNumber = (number, size) => {
+    if (!Number(number)) return;
+    const stars = [];
+    if (+number === 0) {
+        for (let i = 0; i < 5; i++) {
+            stars.push(<FaRegStar color='orange' size={size || 16} />)
+        }
+    }
+    for (let i = 0; i < +number; i++) {
+        stars.push(<FaStar color='orange' size={size || 16} />)
+    }
+    for (let i = 5; i > +number; i--) {
+        stars.push(<FaRegStar color='orange' size={size || 16} />)
+    }
+    return stars;
+}
+
+export const secondsToHms = (seconds) => {
+    seconds = Number(seconds) / 1000;
+    const h = Math.floor(seconds / 3600);
+    const m = Math.floor(seconds % 3600 / 60);
+    const s = Math.floor(seconds % 3600 % 60);
+    return ({ h, m, s });
 }
