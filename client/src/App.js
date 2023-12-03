09e145c7d84d274786a919c25c4509d7c7979f26
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-03 23:18:49 +0700

    define a few more paths in root app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Login, Home, Public } from './pages/public';
+import { Login, Home, Public, FAQs, Service, Blog, DetailProduct, Product } from './pages/public';
 import path from './ultils/path';
-import { getCategories } from './store/asyncAction';
+import { getCategories } from './store/app/asyncAction';
 import { useDispatch } from 'react-redux';
 function App() {
     const dispatch = useDispatch();
@@ -15,8 +15,13 @@ function App() {
             <Routes>
                 <Route path={path.PUBLIC} element={<Public />}>
                     <Route path={path.HOME} element={<Home />} />
-                    <Route path={path.LOGIN} element={<Login />} />
+                    <Route path={path.BLOG} element={<Blog />} />
+                    <Route path={path.DETAIL_PRODUCT__PID__TITLE} element={<DetailProduct />} />
+                    <Route path={path.FAQS} element={<FAQs />} />
+                    <Route path={path.SERVICE} element={<Service />} />
+                    <Route path={path.PRODUCT} element={<Product />} />
                 </Route>
+                <Route path={path.LOGIN} element={<Login />} />
             </Routes>
         </div>
     );
