04d5d4da121eba49d68112f12c51afe6e8bdd4e7
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-05 22:16:15 +0700

    add new final register component

---
+++ client/src/pages/public/FinalRegister.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import path from '../../ultils/path';
+
+
+const FinalRegister = () => {
+    const { status } = useParams();
+    const navigate = useNavigate();
+    useEffect(() => {
+        if (status === 'fail') {
+            Swal.fire('Oops', 'Something went wrong', 'error').then(() => {
+                navigate(`/${path.LOGIN}`);
+            })
+        }
+        if (status === 'success') {
+            Swal.fire('Congratulation', 'Registeration completed. Please Login', 'success').then(() => {
+                navigate(`/${path.LOGIN}`);
+            })
+        }
+    }, [])
+    return (
+        <div className='h-screen w-screen bg-gray-100'>
+
+        </div>
+    )
+}
+
+export default FinalRegister
