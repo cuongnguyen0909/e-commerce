4dfb2bdba4aee5e8f21781610d2ee565fb85d320
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-05 22:09:30 +0700

    define a few mor path and using toastify libray

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Login, Home, Public, FAQs, Service, Blog, DetailProduct, Product } from './pages/public';
+import { Login, Home, Public, FAQs, Service, Blog, DetailProduct, Product, FinalRegister, ResetPassword } from './pages/public';
 import path from './ultils/path';
-import { getCategories } from './store/app/asyncAction';
+import { getCategories } from './store/app/appAction';
 import { useDispatch } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
     const dispatch = useDispatch();
     useEffect(() => {
@@ -16,13 +18,27 @@ function App() {
                 <Route path={path.PUBLIC} element={<Public />}>
                     <Route path={path.HOME} element={<Home />} />
                     <Route path={path.BLOG} element={<Blog />} />
-                    <Route path={path.DETAIL_PRODUCT__PID__TITLE} element={<DetailProduct />} />
+                    <Route path={path.DETAIL_PRODUCT__CATEGORY__PID__TITLE} element={<DetailProduct />} />
                     <Route path={path.FAQS} element={<FAQs />} />
+                    <Route path={path.SERVICE} element={<Service />} />
                     <Route path={path.SERVICE} element={<Service />} />
                     <Route path={path.PRODUCT} element={<Product />} />
                 </Route>
                 <Route path={path.LOGIN} element={<Login />} />
+                <Route path={path.RESET_PASSWORD} element={<ResetPassword />} />
+                <Route path={path.FINAL_REGISTER} element={<FinalRegister />} />
             </Routes>
+            <ToastContainer >
+                posistion="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+            </ToastContainer>
         </div>
     );
 }
