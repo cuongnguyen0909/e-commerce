e59645757af8eaf6b1f74c7449967532d0c534a4
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-05 22:15:38 +0700

    client add new detail product component

--- client/src/pages/public/DetailProduct.jsx
+++ client/src/pages/public/DetailProduct.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apiGetOneProduct } from '../../apis/product';
+import { Breadcrumb } from '../../components';
 
 const DetailProduct = () => {
-    const { pid } = useParams()
+    const { pid, title, category } = useParams();
+    const [productData, setProductData] = useState(null);
+    const fetchProductData = async () => {
+        const response = await apiGetOneProduct(pid);
+        if (response.status) setProductData(response.product);
+    }
+    useEffect(() => {
+        if (pid) fetchProductData();
+    }, [pid])
     return (
-        <div>DetailProduct</div>
+        <div className='w-full '>
+            <div className='h-[81px] flex justify-center items-center bg-gray-100'>
+                <div className='w-main'>
+                    <h3>{title}</h3>
+                    <Breadcrumb title={title} category={category} />
+                </div>
+            </div>
+
+        </div>
     )
 }
 
