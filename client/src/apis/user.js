50cd72327672204697a198eb247299e5fda73082
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-05 22:08:44 +0700

    clent update api user

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -3,10 +3,30 @@ import axios from '../axios';
 export const apiRegister = (data) => axios({
     url: '/user/register',
     method: 'POST',
-    data: data
+    data: data,
+    withCredentials: true
+});
+export const apiFinalRegister = (token) => axios({
+    url: '/user/finalregister/' + token,
+    method: 'PUT',
+    withCredentials: true
 });
 export const apiLogin = (data) => axios({
     url: '/user/login',
     method: 'POST',
     data: data
+});
+export const apiForgotPassword = (data) => axios({
+    url: '/user/forgotpassword',
+    method: 'POST',
+    data: data
+});
+export const apiResetPassword = (data) => axios({
+    url: '/user/resetpassword',
+    method: 'PUT',
+    data: data
+});
+export const apiGetCurrent = () => axios({
+    url: '/user/current',
+    method: 'GET'
 });
