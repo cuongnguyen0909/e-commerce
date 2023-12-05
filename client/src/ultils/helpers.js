1935414727d06baaca0967a70f3452a27fd2923e
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-05 22:10:50 +0700

    client valid unput

--- client/src/ultils/helpers.js
+++ client/src/ultils/helpers.js
@@ -33,3 +33,37 @@ export const secondsToHms = (seconds) => {
     const s = Math.floor(seconds % 3600 % 60);
     return ({ h, m, s });
 }
+
+export const validate = (payload, setInvalidFields) => {
+    let invalidFields = 0;
+    //paylloadformat is array
+    const formatPayload = Object.entries(payload)
+    // console.log(formatPayload);
+    for (let arr of formatPayload) {
+        if (arr[1] === '') {
+            invalidFields++;
+            setInvalidFields(prev => [...prev, { name: arr[0], message: 'This field is required' }]);
+        }
+    }
+    // for (let arr of formatPayload) {
+    //     switch (arr[0]) {
+    //         case 'email':
+    //             const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    //             if (!arr[1].match(regex)) {
+    //                 invalidFields++;
+    //                 setInvalidFields(prev => [...prev, { name: arr[0], message: 'Invalid email' }]);
+    //             }
+    //             break;
+    //         case 'password':
+    //             if (arr[1].length < 6) {
+    //                 invalidFields++;
+    //                 setInvalidFields(prev => [...prev, { name: arr[0], message: 'Password must be at least 6 characters' }]);
+    //             }
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // }
+
+    return invalidFields;
+}
