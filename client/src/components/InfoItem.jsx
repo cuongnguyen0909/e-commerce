7d1b88ac95a311961986a8578022b21e9acb6665
cuongnguyen0909 <nguyenquoccuong2255@gmail.com>
2023-12-08 15:26:32 +0700

    client: create new infor item used in detail poroduct component

---
+++ client/src/components/InfoItem.jsx
@@ -0,0 +1,20 @@
+import React, { memo } from 'react'
+
+const InfoItem = ({ icon, title, sub }) => {
+    return (
+        <div>
+            <div className='flex gap-4 items-center border p-2 mb-[10px] '>
+                <span className='text-white text-[24px] p-2 flex items-center justify-center bg-gray-800 rounded-full z-50'>
+                    {icon}
+                </span>
+                <div className='flex flex-col text-[14px]'>
+                    <span className='font-semibold'>{title}</span>
+                    <span className='text-[12px] text-gray-500'>{sub}</span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default memo(InfoItem)
